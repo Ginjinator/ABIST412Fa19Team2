8713b6253947ad0f6302ef7edc5e5b0688cf57e7
Erik Ellis <elliserik69@gmail.com>
2019-12-03 14:52:28 -0500

    Small additions to check and make appointment controllers

---
+++ Version1/src/Controller/ControllerMakeAppointment.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+public class ControllerMakeAppointment {
+
+    @FXML private DatePicker datePicker;
+    @FXML private ChoiceBox choice;
+    @FXML private ListView list;
+
+    @FXML
+    private void setDate (ActionEvent event) throws IOException {
+
+    }
+}
