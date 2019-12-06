044629c8e895d1e11a467e01559c1e42946c55a3
tvg5197 <a@cd12676>
2019-12-06 00:09:46 -0500

    added professional user or regular user dropdown option to sign up

--- Version1/src/Controller/ControllerSignUp.java
+++ Version1/src/Controller/ControllerSignUp.java
@@ -1,11 +1,16 @@
 package Controller;
 
+import Model.ProfessionalUser;
+import Model.ProfessionalUserData;
 import Model.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
@@ -15,52 +20,72 @@ import javafx.scene.paint.Color;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class ControllerSignUp implements Initializable{
-    @FXML private TextField firstNameField;
-    @FXML private TextField lastNameField;
-    @FXML private TextField usernameField;
-    @FXML private TextField phoneNumField;
-    @FXML private PasswordField passwordField;
-    @FXML private PasswordField passwordConfirmationField;
-    @FXML private Button submitButton;
+public class ControllerSignUp {
+	@FXML
+	private TextField firstNameField;
+	@FXML
+	private TextField lastNameField;
+	@FXML
+	private TextField usernameField;
+	@FXML
+	private TextField phoneNumField;
+	@FXML
+	private PasswordField passwordField;
+	@FXML
+	private PasswordField passwordConfirmationField;
+	@FXML
+	private Button submitButton;
+	@FXML
+	private ChoiceBox professionalUser;
+	//######################SIGNUP SCREEN###########################
 
-    //######################SIGNUP SCREEN###########################
+	@FXML
+	private void submitSignUp(ActionEvent event) throws IOException {
+		//Special Character Verification & Screen Swap
 
+		if (!Controller.hasSpecialChars(firstNameField.getText()) && !Controller.hasSpecialChars(lastNameField.getText()) && !Controller.hasSpecialChars(usernameField.getText()) && !Controller.hasSpecialChars(passwordField.getText()) && !Controller.hasSpecialChars(phoneNumField.getText())) {
+			int selectedIndex = professionalUser.getSelectionModel().getSelectedIndex();
+			if (selectedIndex == 0) {
+				//User creation and added to Database
+				User user = new User(firstNameField.getText(), lastNameField.getText(), usernameField.getText(), passwordField.getText(), phoneNumField.getText());
+				Model.UserData userData = new Model.UserData();
+				userData.addUser(user);
+				System.out.println("Added a regular user");
+			}
 
-    @FXML
-    private void submitSignUp(ActionEvent event) throws IOException {
-        //Special Character Verification & Screen Swap
-        if(!Controller.hasSpecialChars(firstNameField.getText()) && !Controller.hasSpecialChars(lastNameField.getText()) && !Controller.hasSpecialChars(usernameField.getText()) && !Controller.hasSpecialChars(passwordField.getText()) && !Controller.hasSpecialChars(phoneNumField.getText())){
+			if (selectedIndex == 1) {
+				//PROFESSIONAL creation and added to Database
+				ProfessionalUser user = new ProfessionalUser(firstNameField.getText(), lastNameField.getText(), usernameField.getText(), passwordField.getText());
+				Model.ProfessionalUserData profUserData = new Model.ProfessionalUserData();
+				profUserData.addUser(user);
+				System.out.println("Added a professional user");
+			}
+			//Screen Swap
+			Controller.loadScreen("LoginScreen.fxml", event);
+		} else {//Error FeedBack
+			if (Controller.hasSpecialChars(firstNameField.getText())) {
+				firstNameField.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+			}
+			if (Controller.hasSpecialChars(lastNameField.getText())) {
+				lastNameField.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+			}
+			if (Controller.hasSpecialChars(usernameField.getText())) {
+				usernameField.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+			}
+			if (Controller.hasSpecialChars(passwordField.getText())) {
+				passwordField.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+			}
 
-            //User creation and added to Database
-            User user = new User(firstNameField.getText(), lastNameField.getText(), usernameField.getText(), passwordField.getText(), phoneNumField.getText());
-            Model.UserData userData = new Model.UserData();
-            userData.addUser(user);
-
-            //Screen Swap
-            Controller.loadScreen("LoginScreen.fxml", event);
-        }else{//Error FeedBack
-            if(Controller.hasSpecialChars(firstNameField.getText())){
-                firstNameField.setBackground(new Background(new BackgroundFill( Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
-            }
-            if(Controller.hasSpecialChars(lastNameField.getText())){
-                lastNameField.setBackground(new Background(new BackgroundFill( Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
-            }
-            if(Controller.hasSpecialChars(usernameField.getText())){
-                usernameField.setBackground(new Background(new BackgroundFill( Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
-            }
-            if(Controller.hasSpecialChars(passwordField.getText())){
-                passwordField.setBackground(new Background(new BackgroundFill( Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
-            }
-
-            if(!passwordField.getText().equals(passwordConfirmationField.getText())){
-                passwordField.setBackground(new Background(new BackgroundFill( Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
-                passwordConfirmationField.setBackground(new Background(new BackgroundFill( Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
-            }
-        }
-    }
+			if (!passwordField.getText().equals(passwordConfirmationField.getText())) {
+				passwordField.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+				passwordConfirmationField.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+			}
+		}
+	}
 
 //    //######################All Main SCREEN###########################
 //
@@ -73,9 +98,13 @@ public class ControllerSignUp implements Initializable{
 //        stage.show();
 //    }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	public void initialize() {
+		List<String> list = new ArrayList<String>();
+		list.add("User");
+		list.add("Doctor");
+		ObservableList obList = FXCollections.observableList(list);
+		this.professionalUser.setItems(obList);
+		this.professionalUser.getSelectionModel().selectFirst();
+	}
 
 }
