c7af1a3e3366ca2861a56799a662b6610cb0b7dd
Erik Ellis <elliserik69@gmail.com>
2019-12-08 00:53:15 -0500

    Basic ApproveAppointmentScreen.fxml and ControllerApproveAppointment updates to start

---
+++ Version1/src/Controller/ControllerApproveAppointment.java
@@ -0,0 +1,35 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class ControllerApproveAppointment {
+    @FXML private Button approveButton;
+    @FXML private Button denyButton;
+    @FXML private Button goBackButton;
+    @FXML private ListView appointmentsList;
+
+    @FXML
+    public void intialize() throws IOException {
+
+    }
+
+    @FXML
+    private void setApproveAppointment(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    private void setDenyAppointment(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    private void setMainScreen(ActionEvent event) throws IOException {
+        Controller.loadScreen("MainScreenProfUser.fxml", event);
+    }
+}
